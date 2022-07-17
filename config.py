226e3834253f4148701410ba989585487fb53bd2
khushal trivedi <khushahal.trivedi@gslab.com+1-at-551127968046>
2022-07-17 14:44:27 +0530

    minor config added

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Configuration(BaseSettings):
     db_name: str
     db_password: str
-    DATABASE_URL = 'sqlite:///SQl/home/khushal/school_management.db'
+    DATABASE_URL = str
 
     class Config:
         env_file = '.env'
--- school_management/models.py
+++ school_management/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 import datetime
-from .database import Model
+from .database import Base
 
 
-class BaseModel(Model):
-    created_at = Column(DateTime(timezone=True),
-                        server_default=datetime.datetime.now())
+# class BaseModel(Model): pass
 
 
-class Student(BaseModel):
+# created_at = Column(DateTime(timezone=True),
+#                     server_default=datetime.datetime.now())
+
+
+class Student(Base):
     __tablename__ = "user"
+    id = Column(Integer, primary_key=True, index=True)
